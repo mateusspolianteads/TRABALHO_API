d6f328f42bf91dd6c1faf34c8259273cd59c2bb6
MateusSpoliante <mateus.spoliant11@gmail.com>
2026-05-05 21:45:13 -0300

    Criação da interface de cadastro do usuario (by: Gabriel)

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routes import usuarios
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
